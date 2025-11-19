59d2b234afd9c1919aa4710b2505345d1e206079
Awesomepop7 <estebangomeslopez127@gmail.com>
2025-11-19 18:00:23 -0500

    commited changes

--- calculator.py
+++ calculator.py
@@ -1,11 +1,26 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+#https://github.com/Awesomepop7/lab11-EGL
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+def hypotenuse(a,b):
+    return math.hypot(a,b)
 
-
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+def exp(a, b):
+    return a ** b
